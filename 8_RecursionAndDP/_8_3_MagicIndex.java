7b47835e93379896368d07f6f86405a4bf1279dc
rayhan <hossain.rayhan@outlook.com>
2019-02-22 00:42:38 -0500

    CTCI-8.3: Magic Index where arr[i] = i

--- 8_RecursionAndDP/_8_3_MagicIndex.java
+++ 8_RecursionAndDP/_8_3_MagicIndex.java
@@ -9,11 +9,12 @@ import java.util.Arrays;
 
 class MagicIndex{
 	public static void main(String[] args){
-		int[] arr = {-40, -20, -1, 1, 2, 3, 5, 7, 9, 12, 13};
+		int[] arr = {-40, -20, -1, 1, 2, 3, 5, 7, 9, 12, 13};//all distinct
+		int[] arr2 = {-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 12};
 
-		System.out.println(Arrays.toString(arr));
-		int index = magicIndex(arr, 0, arr.length - 1);
-		System.out.println("Magic Index- All distinct values:" + index);
+		System.out.println(Arrays.toString(arr2));
+		int index = magicIndexFollowUp(arr2, 0, arr2.length - 1);
+		System.out.println("Magic Index: " + index);
 	}
 
 	//use the concept of binary search- all distinct values
@@ -29,6 +30,34 @@ class MagicIndex{
 		}else{
 			return magicIndex(arr, mid + 1, end);
 		}
+	}
+
+	//values are not distinct
+	public static int magicIndexFollowUp(int[] arr, int start, int end){
+		if(end < start){
+			return -1;
+		}
+
+		int midIndex = (start + end)/2;
+		int midValue = arr[midIndex];
+
+		if(midIndex == midValue){
+			return midIndex;
+		}
+
+		//search left
+		int leftIndex = Math.min(midIndex-1, midValue);
+		int left = magicIndexFollowUp(arr, start, leftIndex);
+		if(left >= 0){
+			return left;
+		}
+
+		//search right
+		int rightIndex = Math.max(midIndex+1, midValue);
+		int right = magicIndexFollowUp(arr, rightIndex, end);
+		
+		return right;
+	
 	}
 }
 
